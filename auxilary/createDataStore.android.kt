849572dff04c0eee2ad19dd7f63ef6c82b4dfda0
Márton Braun <marton.braun@jetbrains.com>
2024-06-27 14:50:09 +0200

    Add aux files

---
+++ auxilary/createDataStore.android.kt
@@ -0,0 +1,7 @@
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+
+fun createDataStore(context: Context): DataStore<Preferences> = createDataStore(
+    producePath = { context.filesDir.resolve(dataStoreFileName).absolutePath }
+)
