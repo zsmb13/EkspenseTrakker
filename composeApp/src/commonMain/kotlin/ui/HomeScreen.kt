1f127cea140e10c88ed60969e70bb41790f14005
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-06-27 15:32:19 +0200

    Scaffold HomeScreen.kt

---
+++ composeApp/src/commonMain/kotlin/ui/HomeScreen.kt
@@ -0,0 +1,41 @@
+package ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import ekspensetrakker.composeapp.generated.resources.*
+import ekspensetrakker.composeapp.generated.resources.Res
+import ekspensetrakker.composeapp.generated.resources.add_expense
+import org.jetbrains.compose.resources.stringResource
+import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.annotation.KoinExperimentalAPI
+
+@OptIn(KoinExperimentalAPI::class)
+@Composable
+fun HomeScreen() {
+    Column(Modifier.fillMaxSize().padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        LazyColumn(Modifier.fillMaxWidth().weight(1f)) {
+            
+        }
+        Spacer(
+            Modifier.padding(5.dp).height(2.dp).fillMaxWidth().background(MaterialTheme.colors.primary)
+        )
+        Text("Total: ?", fontSize = 24.sp, modifier = Modifier.padding(16.dp))
+        Button(onClick = { }) {
+            
+        }
+    }
+}
